9464f56cb4688baa10f19da04b32956f2153a11f
rahulabrol2211 <rahulabrol2211@gmail.com>
2021-03-15 18:45:11 +0530

    refactoring

--- app/src/main/java/com/rahulabrol/smsreader/HomeViewModel.kt
+++ app/src/main/java/com/rahulabrol/smsreader/HomeViewModel.kt
@@ -1,12 +1,18 @@
 package com.rahulabrol.smsreader
 
-import androidx.annotation.MainThread
-import androidx.databinding.ObservableBoolean
-import androidx.lifecycle.MutableLiveData
+import android.util.Log
 import com.rahulabrol.smsreader.base.LiveCoroutinesViewModel
+import com.rahulabrol.smsreader.model.DateItem
+import com.rahulabrol.smsreader.model.GeneralItem
+import com.rahulabrol.smsreader.model.ListItem
+import com.rahulabrol.smsreader.model.Message
 import com.rahulabrol.smsreader.repository.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
+import kotlin.math.abs
 
 /**
  * Created by Rahul Abrol on 14/3/21.
@@ -15,49 +21,84 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(private var homeRepository: HomeRepository) :
     LiveCoroutinesViewModel() {
 
-    var isLoading: ObservableBoolean = ObservableBoolean(false)
-    var toastLiveData: MutableLiveData<String> = MutableLiveData()
+    /**
+     * Return date in specified format.
+     * @param milliSeconds Date in milliseconds
+     * @param dateFormat Date format
+     * @return String representing date in specified format
+     */
+    fun getDate(milliSeconds: Long): String {
+        // Create a DateFormatter object for displaying date in specified format.
+        val formatter = SimpleDateFormat("dd/MM/yyyy hh:mm:ss", Locale.getDefault())
 
-//    var postsLiveData: LiveData<List<Post>?>
-    private var postMutualLiveData: MutableLiveData<Boolean> = MutableLiveData()
-
-//    var postDetailLiveData: LiveData<PostDetail?>
-    private var postDetailMutualLiveData: MutableLiveData<String> = MutableLiveData()
-
-    init {
-//        postsLiveData = postMutualLiveData.switchMap {
-//            isLoading.set(it)
-//            launchOnViewModelScope {
-//                this.homeRepository.getPosts({ size ->
-//                    isLoading.set(false)
-//                }, {
-//                    isLoading.set(false)
-//                    toastLiveData.postValue(it)
-//                }).asLiveData()
-//            }
-//        }
-
-//        postDetailLiveData = postDetailMutualLiveData.switchMap {
-//            isLoading.set(true)
-//            launchOnViewModelScope {
-//                this.homeRepository.getPostDetail(it, {
-//                    isLoading.set(false)
-//                }, {
-//                    isLoading.set(false)
-//                    toastLiveData.postValue(it)
-//                }).asLiveData()
-//            }
-//        }
+        // Create a calendar object that will convert the date and time value in milliseconds to date.
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = milliSeconds
+        return formatter.format(calendar.time)
     }
 
-    @MainThread
-    fun getPostList() {
-        postMutualLiveData.value = true
+    fun getExtractedList(smsList: MutableList<Message>): ArrayList<ListItem> {
+        val groupedHashMap: HashMap<String, List<ListItem>> = LinkedHashMap()
+        smsList.forEach {
+            val daysAgo = getDifference(it._time)
+            val generalItem =
+                GeneralItem(it._address, it._msg, getDate(it._time.time))
+            if (groupedHashMap.containsKey(daysAgo)) {
+                (groupedHashMap[daysAgo] as ArrayList<GeneralItem>).add(generalItem)
+            } else {
+                val arrayList = arrayListOf<GeneralItem>()
+                arrayList.add(generalItem)
+                groupedHashMap[daysAgo] = arrayList
+            }
+        }
+
+        // We linearly add every item into the consolidatedList.
+        val consolidatedList = arrayListOf<ListItem>()
+        for (date in groupedHashMap.keys) {
+            val dateItem = DateItem(date)
+            consolidatedList.add(dateItem)
+            for (generalItem in groupedHashMap[date]!!) {
+                consolidatedList.add(generalItem)
+            }
+        }
+        return consolidatedList
     }
 
-    @MainThread
-    fun getUserDetail(userId: String?) {
-        postDetailMutualLiveData.value = userId
-    }
+    private fun getDifference(endDate: Date): String {
+        val startDate = Date(System.currentTimeMillis())
 
+        val duration: Long = endDate.time - startDate.time
+
+        val diffInHours = TimeUnit.MILLISECONDS.toHours(duration)
+        val diffInDays = TimeUnit.MILLISECONDS.toDays(duration)
+        var count = ""
+        if (diffInDays == 0L) {
+            when (Math.abs(diffInHours)) {
+                0L -> {
+                    count = "0 hours ago"
+                }
+                1L -> {
+                    count = "1 hours ago"
+                }
+                2L -> {
+                    count = "2 hours ago"
+                }
+                3L -> {
+                    count = "3 hours ago"
+                }
+                4L, 5L, 6L -> {
+                    count = "6 hours ago"
+                }
+                7L, 8L, 9L, 10L, 11L, 12L -> {
+                    count = "12 hours ago"
+                }
+                else -> {
+                    count = "1 day ago"
+                }
+            }
+        } else {
+            count = "${abs(diffInDays)} days ago"
+        }
+        return count
+    }
 }
--- app/src/main/java/com/rahulabrol/smsreader/ui/smslist/SmsListFragment.kt
+++ app/src/main/java/com/rahulabrol/smsreader/ui/smslist/SmsListFragment.kt
@@ -31,6 +31,7 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 import kotlin.collections.LinkedHashMap
+import kotlin.math.abs
 
 
 /**
@@ -119,92 +120,8 @@ class SmsListFragment : DataBindingFragment() {
 
     private fun update() {
         val smsList = readSmsFromPhone()
-        Log.e("SMS READER", "SMS LIST Count: ${smsList.size}")
-        val extractedList = getExtractedList(smsList)
+        val extractedList = homeViewModel.getExtractedList(smsList)
         postAdapter.addSmsList(extractedList)
-    }
-
-    /**
-     * Return date in specified format.
-     * @param milliSeconds Date in milliseconds
-     * @param dateFormat Date format
-     * @return String representing date in specified format
-     */
-    private fun getDate(milliSeconds: Long, dateFormat: String?): String {
-        // Create a DateFormatter object for displaying date in specified format.
-        val formatter = SimpleDateFormat(dateFormat, Locale.getDefault())
-
-        // Create a calendar object that will convert the date and time value in milliseconds to date.
-        val calendar = Calendar.getInstance()
-        calendar.timeInMillis = milliSeconds
-        return formatter.format(calendar.time)
-    }
-
-    private fun getExtractedList(smsList: MutableList<Message>): ArrayList<ListItem> {
-        val groupedHashMap: HashMap<String, List<ListItem>> = LinkedHashMap()
-        smsList.forEach {
-            val daysAgo = getDifference(it._time)
-            val generalItem =
-                GeneralItem(it._address, it._msg, getDate(it._time.time, "dd/MM/yyyy hh:mm:ss"))
-            if (groupedHashMap.containsKey(daysAgo)) {
-                (groupedHashMap[daysAgo] as ArrayList<GeneralItem>).add(generalItem)
-            } else {
-                val arrayList = arrayListOf<GeneralItem>()
-                arrayList.add(generalItem)
-                groupedHashMap[daysAgo] = arrayList
-            }
-        }
-
-        Log.e("SMS READER", "map size: ${groupedHashMap.size}")
-
-        // We linearly add every item into the consolidatedList.
-        val consolidatedList = arrayListOf<ListItem>()
-        for (date in groupedHashMap.keys) {
-            val dateItem = DateItem(date)
-            consolidatedList.add(dateItem)
-            for (generalItem in groupedHashMap[date]!!) {
-                consolidatedList.add(generalItem)
-            }
-        }
-        return consolidatedList
-    }
-
-    private fun getDifference(endDate: Date): String {
-        val startDate = Date(System.currentTimeMillis())
-
-        val duration: Long = endDate.time - startDate.time
-
-        val diffInHours = TimeUnit.MILLISECONDS.toHours(duration)
-        val diffInDays = TimeUnit.MILLISECONDS.toDays(duration)
-        var count = ""
-        if (diffInDays == 0L) {
-            when (Math.abs(diffInHours)) {
-                0L -> {
-                    count = "0 hours ago"
-                }
-                1L -> {
-                    count = "1 hours ago"
-                }
-                2L -> {
-                    count = "2 hours ago"
-                }
-                3L -> {
-                    count = "3 hours ago"
-                }
-                4L, 5L, 6L -> {
-                    count = "6 hours ago"
-                }
-                7L, 8L, 9L, 10L, 11L, 12L -> {
-                    count = "12 hours ago"
-                }
-                else -> {
-                    count = "1 day ago"
-                }
-            }
-        } else {
-            count = "${Math.abs(diffInDays)} days ago"
-        }
-        return count
     }
 
     private fun readSmsFromPhone(): MutableList<Message> {
@@ -216,7 +133,7 @@ class SmsListFragment : DataBindingFragment() {
             null,
             null
         )
-        var totalSMS = 0
+        val totalSMS: Int
         if (c != null) {
             totalSMS = c.count
             if (c.moveToFirst()) {
@@ -233,7 +150,6 @@ class SmsListFragment : DataBindingFragment() {
                         else -> {
                         }
                     }
-//                    Log.e("SMS READED", "Number: $number Message: $body Time: $smsDate")
                     val message = Message(number, body, dateFormat)
                     smsList.add(message)
                     c.moveToNext()
@@ -245,7 +161,6 @@ class SmsListFragment : DataBindingFragment() {
         }
         return smsList
     }
-
 
     companion object {
         const val TAG = "SmsListFragment"
